4b564170fd83980f1e349373eff33286752804c2
Nicholas Appert <nappert@student.sierracollege.edu>
2020-12-09 16:42:05 -0800

    Added logic for pulling parameters from request

--- movie_search_page/views.py
+++ movie_search_page/views.py
@@ -8,23 +8,48 @@ from .models import Movie
 
 # Create your views here.
 
-def index(request, categoryname, moviename):
+def index(request, categoryname='all', moviename='all'):
     request_url = 'http://127.0.0.1:5000/movies'
 
-    if categoryname == "COSTUME-DESIGN-Color":
+    if categoryname == "COSTUME-DESIGN-COLOR":
         categoryname = "COSTUME DESIGN (Color)"
-    elif categoryname == "COSTUME-DESIGN-Black-and-White":
+    elif categoryname == "COSTUME-DESIGN-BLACK-AND-WHITE":
         categoryname = "COSTUME DESIGN (Black-and-White)"
     elif categoryname == "MAKEUP-AND-HAIRSTYLING":
         categoryname = "MAKEUP AND HAIRSTYLING"
 
-    if moviename != 'all':
+    if moviename != 'FindMyOscar_Everything' and moviename != 'all':
         request_url += '/title/' + moviename
-    elif categoryname != 'all':
+    elif categoryname != 'FindMyOscar_Everything' and moviename != 'all':
         request_url += '/category/' + categoryname
 
+        portion_count = 1
+        year = ''
+        winner = ''
+
+        for letter in request:
+            if letter == 'z':
+                portion_count += 1
+            elif portion_count == 2:
+                if letter != 'z':
+                    year += letter
+                else:
+                    portion_count += 1
+            elif portion_count == 3:
+                if letter != '/':
+                    winner += letter
+                else:
+                    portion_count += 1
+            else:
+                pass
+
+            if year != '' or winner != '':
+                request_url += '?year=' + year
+                if winner == 'winner':
+                    request_url += '&winner=True'
+
     response = requests.get(request_url)
-    if moviename != 'all':
+    if moviename != 'FindMyOscar_Everything' and moviename != 'all':
         attributes = [json.loads(response.text)]
     else:
         attributes = json.loads(response.text)
